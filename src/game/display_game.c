14e844865e1c7b6c099a58aea7b4944c8be8262b
louiscapelle <louis.capelle@epitech.eu>
2020-02-19 16:34:13 +0100

    organization optimized, next step is to fix tower rotation, creating branch is needed to do this work

--- src/game/display_game.c
+++ src/game/display_game.c
@@ -7,20 +7,6 @@
 
 #include "my.h"
 
-void init_towers(game_t *game)
-{
-    sfVector2f pos_tower1 = {330, 540};
-    sfVector2f origin = {64, 64};
-
-    game->terrain->tower1_sprite = sfSprite_create();
-    game->terrain->tower1_texture = sfTexture_createFromFile
-                                ("./utils/imgs/tower1.png", NULL);
-    sfSprite_setTexture(game->terrain->tower1_sprite,
-                        game->terrain->tower1_texture, sfTrue);
-    sfSprite_setPosition(game->terrain->tower1_sprite, pos_tower1);
-    sfSprite_setOrigin(game->terrain->tower1_sprite, origin);
-}
-
 void init_terrain(game_t *game)
 {
     init_towers(game);
@@ -47,24 +33,6 @@ int init_enemys(game_t *game, sfClock *clock)
         sfSprite_setPosition(game->enemys->sprite, game->enemys->pos);
     }
     return 0;
-}
-
-void rotate_tower(game_t *game)
-{
-    sfTime time;
-    float seconds = 0;
-    float position = sfSprite_getRotation(game->terrain->tower1_sprite);
-
-    time = sfClock_getElapsedTime(game->terrain->clock);
-    seconds = time.microseconds / 100000.0;
-    if (seconds > 4.5) {
-        if (position >= 3) {
-            sfSprite_rotate(game->terrain->tower1_sprite, position - 0.1);
-        } else if (position >= -25) {
-            sfSprite_rotate(game->terrain->tower1_sprite, position + 0.1);
-        }
-        sfClock_restart(game->terrain->clock);
-    }
 }
 
 void display_game(sfRenderWindow *window, game_t *game)
