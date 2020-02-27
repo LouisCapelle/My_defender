b4b9adf166e706e92835b3291e6cab6c1797b867
louiscapelle <louis.capelle@epitech.eu>
2020-02-27 22:47:51 +0100

    positions of sprites has been updated

--- src/game/towers/init_base_towers.c
+++ src/game/towers/init_base_towers.c
@@ -9,17 +9,15 @@
 
 int set_positions_base(game_t *game)
 {
-    sfVector2f pos_1 = {50, 50};
-    sfVector2f pos_2 = {100, 100};
-    sfVector2f pos_3 = {150, 150};
-    sfVector2f pos_4 = {200, 200};
-    sfVector2f pos_5 = {250, 250};
+    sfVector2f pos_1 = {310, 110};
+    sfVector2f pos_2 = {355, 510};
+    sfVector2f pos_3 = {650, 250};
+    sfVector2f pos_4 = {850, 380};
 
     sfSprite_setPosition(game->towers->base1_sprite, pos_1);
     sfSprite_setPosition(game->towers->base2_sprite, pos_2);
     sfSprite_setPosition(game->towers->base3_sprite, pos_3);
     sfSprite_setPosition(game->towers->base4_sprite, pos_4);
-    sfSprite_setPosition(game->towers->base5_sprite, pos_5);
 }
 
 int set_scales(game_t *game)
@@ -30,7 +28,6 @@ int set_scales(game_t *game)
     sfSprite_setScale(game->towers->base2_sprite, scale);
     sfSprite_setScale(game->towers->base3_sprite, scale);
     sfSprite_setScale(game->towers->base4_sprite, scale);
-    sfSprite_setScale(game->towers->base5_sprite, scale);
 }
 
 int create_sprites(game_t *game)
@@ -39,7 +36,6 @@ int create_sprites(game_t *game)
     game->towers->base2_sprite = sfSprite_create();
     game->towers->base3_sprite = sfSprite_create();
     game->towers->base4_sprite = sfSprite_create();
-    game->towers->base5_sprite = sfSprite_create();
     sfSprite_setTexture(game->towers->base1_sprite,
                         game->towers->base_texture, sfTrue);
     sfSprite_setTexture(game->towers->base2_sprite,
@@ -47,8 +43,6 @@ int create_sprites(game_t *game)
     sfSprite_setTexture(game->towers->base3_sprite,
                         game->towers->base_texture, sfTrue);
     sfSprite_setTexture(game->towers->base4_sprite,
-                        game->towers->base_texture, sfTrue);
-    sfSprite_setTexture(game->towers->base5_sprite,
                         game->towers->base_texture, sfTrue);
 }
 
