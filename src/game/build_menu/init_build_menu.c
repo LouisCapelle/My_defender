b5475ac5a2252edbcf7c0786c88dee9834cc0d9b
Louis <louis.capelle@epitech.eu>
2020-03-02 22:52:02 +0100

    many towers has been uploaded, beggining build menu work, i continue tomorrow

--- src/game/build_menu/init_build_menu.c
+++ src/game/build_menu/init_build_menu.c
@@ -7,6 +7,13 @@
 
 #include "my.h"
 
+int init_build_icons(game_t *game)
+{
+    sfVector2f pos_1 = {1, 1};
+    sfVector2f pos_2 = {2, 2};
+    sfVector2f pos_3 = {3, 3};
+}
+
 int init_build_menu(game_t *game)
 {
     sfVector2f pos = {1000, 30};
@@ -28,4 +35,5 @@ int init_build_menu(game_t *game)
     sfSprite_setPosition(game->build_menu->blank_sprite, pos_blank);
     sfSprite_setScale(game->build_menu->build_sprite, scale);
     sfSprite_setScale(game->build_menu->blank_sprite, scale_blank);
+    init_build_icons(game);
 }
